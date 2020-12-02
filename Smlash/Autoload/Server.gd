99b662f722ac80c30732bb2f05fcd0c72b119f2b
Lilith-In-Starlight <veromora0712@gmail.com>
2020-12-02 13:56:18 -0500

    Smlash, the Mini Multiplayer Online Role Playing Game
    Name is a joke.

--- Smlash/Autoload/Server.gd
+++ Smlash/Autoload/Server.gd
@@ -1,57 +1,57 @@
 extends Node
 
 var host := false
-var players := 0
+remotesync var players := 0
 var local_id := -1
-var players_inputs := {
-	0: [false, false, false, false]
-}
+remotesync var inputs_0 := [false, false, false, false]
+remotesync var inputs_1 := [false, false, false, false]
+remotesync var inputs_2 := [false, false, false, false]
+remotesync var inputs_3 := [false, false, false, false]
 
 func start_host():
 	host = true
 	var peer := NetworkedMultiplayerENet.new()
 	peer.create_server(5555, 8)
 	get_tree().network_peer = peer
-	get_tree().connect("connected_to_server", self, "on_connected_ok")
-	get_tree().connect("connection_failed", self, "on_connected_fail")
-	get_tree().connect("server_disconnected", self, "on_server_disconnected")
 	local_id = 0
+	get_tree().connect("network_peer_connected", self, "on_entity_join")
+	get_tree().connect("network_peer_disconnected", self, "on_entity_leave")
+	print("Host")
 
 func join(ip):
-	host = true
+	host = false
 	var peer := NetworkedMultiplayerENet.new()
 	peer.create_client(ip, 5555)
 	get_tree().network_peer = peer
-	get_tree().connect("network_peer_connected", self, "on_entity_join")
-	get_tree().connect("network_peer_disconnected", self, "on_entity_leave")
-
+	get_tree().connect("connected_to_server", self, "on_connected_ok")
+	get_tree().connect("connection_failed", self, "on_connected_fail")
+	get_tree().connect("server_disconnected", self, "on_server_disconnected")
+	print("Client")
 
 # Server functions
-func on_entity_join():
-	pass
+func on_entity_join(id):
+	print("Player joined with id: " + str(id))
 
-func on_entity_leave():
-	pass
+func on_entity_leave(id):
+	print("Player left with id: " + str(id))
 
-func register_player():
-	players += 1
-	rpc("get_playerq_from_server", players)
+remote func register_player():
+	rset("players", players + 1)
 	rpc_id(get_tree().get_rpc_sender_id(), "set_player_local_player", players)
 
 # Cllient functions
 func on_connected_ok():
 	rpc_id(1, "register_player")
+	print("Conncted")
 
 func on_server_disconnected():
-	pass
+	print("Server disconnected")
 	
 func on_connected_fail():
 	get_tree().disconnect("connected_to_server", self, "on_connected_ok")
 	get_tree().disconnect("connection_failed", self, "on_connected_fail")
 	get_tree().disconnect("server_disconnected", self, "on_server_disconnected")
+	print("Fail")
 
-func get_playerq_from_server(pq):
-	players = pq
-
-func set_player_local_player(lpid):
+remote func set_player_local_player(lpid):
 	local_id = lpid
--- Smlash/Scenes/Lobby.gd
+++ Smlash/Scenes/Lobby.gd
@@ -9,19 +9,117 @@ func _ready():
 func _process(delta):
 	if not lobby_started:
 		if Input.is_key_pressed(KEY_H):
+			Server.start_host()
 			lobby_started = true
 			
 		elif Input.is_key_pressed(KEY_C):
+			Server.join("fdfd::1af4:a824")
 			lobby_started = true
 			
 	elif not game_started:
 		if Input.is_key_pressed(KEY_S):
 			rpc("setup")
+	
+	elif not Server.host:
+		var pl
+		match Server.local_id:
+			0:
+				pl = Server.inputs_0.duplicate()
+			1:
+				pl = Server.inputs_1.duplicate()
+			2:
+				pl = Server.inputs_2.duplicate()
+			3:
+				pl = Server.inputs_3.duplicate()
+		pl[0] = Input.is_key_pressed(KEY_RIGHT)
+		pl[1] = Input.is_key_pressed(KEY_LEFT)
+		pl[2] = Input.is_key_pressed(KEY_UP)
+		pl[3] = Input.is_key_pressed(KEY_DOWN)
+		match Server.local_id:
+			0:
+				Server.rset_unreliable("inputs_0", pl)
+				if pl[0]:
+					get_node("PLAYER_0").rect_position.x += 5
+				if pl[1]:
+					get_node("PLAYER_0").rect_position.x -= 5
+				if pl[2]:
+					get_node("PLAYER_0").rect_position.y -= 5
+				if pl[3]:
+					get_node("PLAYER_0").rect_position.y += 5
+			1:
+				Server.rset_unreliable("inputs_1", pl)
+				if pl[0]:
+					get_node("PLAYER_1").rect_position.x += 5
+				if pl[1]:
+					get_node("PLAYER_1").rect_position.x -= 5
+				if pl[2]:
+					get_node("PLAYER_1").rect_position.y -= 5
+				if pl[3]:
+					get_node("PLAYER_1").rect_position.y += 5
+			2:
+				Server.rset_unreliable("inputs_2", pl)
+				if pl[0]:
+					get_node("PLAYER_2").rect_position.x += 5
+				if pl[1]:
+					get_node("PLAYER_2").rect_position.x -= 5
+				if pl[2]:
+					get_node("PLAYER_2").rect_position.y -= 5
+				if pl[3]:
+					get_node("PLAYER_2").rect_position.y += 5
+			3:
+				Server.rset_unreliable("inputs_3", pl)
+				if pl[0]:
+					get_node("PLAYER_3").rect_position.x += 5
+				if pl[1]:
+					get_node("PLAYER_3").rect_position.x -= 5
+				if pl[2]:
+					get_node("PLAYER_3").rect_position.y -= 5
+				if pl[3]:
+					get_node("PLAYER_3").rect_position.y += 5
+	else:
+		print(Server.players)
+		Server.inputs_0[0] = Input.is_key_pressed(KEY_RIGHT)
+		Server.inputs_0[1] = Input.is_key_pressed(KEY_LEFT)
+		Server.inputs_0[2] = Input.is_key_pressed(KEY_UP)
+		Server.inputs_0[3] = Input.is_key_pressed(KEY_DOWN)
+		if Server.inputs_0[0]:
+			get_node("PLAYER_0").rect_position.x += 5
+		if Server.inputs_0[1]:
+			get_node("PLAYER_0").rect_position.x -= 5
+		if Server.inputs_0[2]:
+			get_node("PLAYER_0").rect_position.y -= 5
+		if Server.inputs_0[3]:
+			get_node("PLAYER_0").rect_position.y += 5
+		
+		if Server.players >= 0:
+			if Server.inputs_1[0]:
+				get_node("PLAYER_1").rect_position.x += 5
+			if Server.inputs_1[1]:
+				get_node("PLAYER_1").rect_position.x -= 5
+			if Server.inputs_1[2]:
+				get_node("PLAYER_1").rect_position.y -= 5
+			if Server.inputs_1[3]:
+				get_node("PLAYER_1").rect_position.y += 5
+		
+		if Server.players > 2:
+			if Server.inputs_2[0]:
+				get_node("PLAYER_2").rect_position.x += 5
+			if Server.inputs_2[1]:
+				get_node("PLAYER_2").rect_position.x -= 5
+			if Server.inputs_2[2]:
+				get_node("PLAYER_2").rect_position.y -= 5
+			if Server.inputs_2[3]:
+				get_node("PLAYER_2").rect_position.y += 5
+		rpc_unreliable("update_pos_on_clients", "PLAYER_0", get_node("PLAYER_0").rect_position)
+		rpc_unreliable("update_pos_on_clients", "PLAYER_1", get_node("PLAYER_1").rect_position)
 
 remotesync func setup():
-	for i in Server.players:
+	for i in Server.players + 1:
 		var player = preload("res://Fighter.tscn").instance()
 		player.name = "PLAYER_" + str(i)
-		player.position.x = 20 + i*30
+		player.rect_position.x = 20 + i*30
 		add_child(player)
 	game_started = true
+
+remotesync func update_pos_on_clients(player, pos):
+	get_node(player).rect_position = pos
