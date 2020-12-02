96d81babe28ca43aee9e8a3f113d8932089eed95
Lilith-In-Starlight <veromora0712@gmail.com>
2020-12-02 12:09:51 -0500

    Added functional lobby

--- Smlash/Autoload/Server.gd
+++ Smlash/Autoload/Server.gd
@@ -1,6 +1,11 @@
 extends Node
 
 var host := false
+var players := 0
+var local_id := -1
+var players_inputs := {
+	0: [false, false, false, false]
+}
 
 func start_host():
 	host = true
@@ -10,6 +15,7 @@ func start_host():
 	get_tree().connect("connected_to_server", self, "on_connected_ok")
 	get_tree().connect("connection_failed", self, "on_connected_fail")
 	get_tree().connect("server_disconnected", self, "on_server_disconnected")
+	local_id = 0
 
 func join(ip):
 	host = true
@@ -27,10 +33,14 @@ func on_entity_join():
 func on_entity_leave():
 	pass
 
+func register_player():
+	players += 1
+	rpc("get_playerq_from_server", players)
+	rpc_id(get_tree().get_rpc_sender_id(), "set_player_local_player", players)
 
 # Cllient functions
 func on_connected_ok():
-	pass
+	rpc_id(1, "register_player")
 
 func on_server_disconnected():
 	pass
@@ -40,3 +50,8 @@ func on_connected_fail():
 	get_tree().disconnect("connection_failed", self, "on_connected_fail")
 	get_tree().disconnect("server_disconnected", self, "on_server_disconnected")
 
+func get_playerq_from_server(pq):
+	players = pq
+
+func set_player_local_player(lpid):
+	local_id = lpid
--- Smlash/Scenes/Lobby.gd
+++ Smlash/Scenes/Lobby.gd
@@ -1,7 +1,7 @@
 extends Control
 
-
 var lobby_started := false
+var game_started := false
 
 func _ready():
 	pass
@@ -13,4 +13,15 @@ func _process(delta):
 			
 		elif Input.is_key_pressed(KEY_C):
 			lobby_started = true
+			
+	elif not game_started:
+		if Input.is_key_pressed(KEY_S):
+			rpc("setup")
 
+remotesync func setup():
+	for i in Server.players:
+		var player = preload("res://Fighter.tscn").instance()
+		player.name = "PLAYER_" + str(i)
+		player.position.x = 20 + i*30
+		add_child(player)
+	game_started = true
